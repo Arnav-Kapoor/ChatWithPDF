56946b9674beac837c24abc73b5b50dca8f0d879
Arnav Kapoor <arnavkapoor1005@gmail.com>
2025-11-27 13:32:52 +0530

    [FIX]:http and grpc port error fixed

--- vectordb_connection.py
+++ vectordb_connection.py
@@ -9,9 +9,11 @@ def set_connection():
     client = weaviate.WeaviateClient(connection_params=ConnectionParams.from_params(
         http_host="amazing-stays-extras-intake.trycloudflare.com",
         # http_port="8080",
+        http_port=None,
         http_secure=True,
         grpc_host="amazing-stays-extras-intake.trycloudflare.com",
         # grpc_port="50051",
+        grpc_port=None,
         grpc_secure=True,
     ))
 
