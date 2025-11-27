8aa3e6d87bae3152bc137274b71ab3b8e3f20605
Arnav Kapoor <arnavkapoor1005@gmail.com>
2025-11-27 13:46:22 +0530

    [FIX]: weaviate connection fix

--- vectordb_connection.py
+++ vectordb_connection.py
@@ -6,16 +6,17 @@ from langchain_weaviate import WeaviateVectorStore
 
 
 def set_connection():
-    client = weaviate.WeaviateClient(connection_params=ConnectionParams.from_params(
-        http_host="amazing-stays-extras-intake.trycloudflare.com",
-        # http_port="8080",
-        http_port=None,
-        http_secure=True,
-        grpc_host="amazing-stays-extras-intake.trycloudflare.com",
-        # grpc_port="50051",
-        grpc_port=None,
-        grpc_secure=True,
-    ))
+    # client = weaviate.WeaviateClient(connection_params=ConnectionParams.from_params(
+    #     http_host="amazing-stays-extras-intake.trycloudflare.com",
+    #     # http_port="8080",
+    #     http_port=None,
+    #     http_secure=True,
+    #     grpc_host="amazing-stays-extras-intake.trycloudflare.com",
+    #     # grpc_port="50051",
+    #     grpc_port=None,
+    #     grpc_secure=True,
+    # ))
+    client=weaviate.Client(url="https://amazing-stays-extras-intake.trycloudflare.com")
 
     client.connect()
     print(client.is_ready())
